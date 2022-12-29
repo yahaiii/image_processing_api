41bae99b35c5ec806e0a6b7eaaa195f6fe6eaa3b
yahaiii <wanekaxa@gmail.com>
2022-12-29 16:41:32 +0100

    setup endpoint test template

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,5 +1,25 @@
-import myFunc from "../index";
+import app from '../index';
+import supertest from 'supertest';
 
-it("expect myFunc(5) to equal 25", () => {
-  expect(myFunc(5)).toEqual(25);
+const request: supertest.SuperTest<supertest.Test> = supertest(app);
+
+// ENDPOINT CHECKER SUITE
+describe('Check all endpoints', (): void => {
+    describe(' GET /api ', (): void => {
+        it('should respond with OK', async (): Promise<void> => {
+            const response: supertest.Response = await request.get('/api');
+            expect(response.statusCode).toBe(200);
+        });
+    });
 });
+
+// ENDPOINT CHECKER SUITE
+describe('Check all endpoints', (): void => {
+    describe(' GET /api ', (): void => {
+        it('should respond with OK', async (): Promise<void> => {
+            const response: supertest.Response = await request.get('/api');
+            expect(response.statusCode).toBe(200);
+        });
+    });
+});
+
